cbc09bb8ac3c85953bf8d65ffba7675f83de3ea7
Keiji Matsuzaki <kmatsuzaki@aiming-inc.com>
2014-01-07 12:32:10 +0900

    Versioning: v0.2.0.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "perlbrew"
 maintainer       "Aiming Inc."
 maintainer_email "info@aiming-inc.com"
 license          "Apache 2.0"
-version          "0.1.0"
+version          "0.2.0"
 description      "Installs/Configures perlbrew"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 recipe           "perlbrew::default", "Installs/updates perlbrew"
